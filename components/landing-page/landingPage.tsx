cb3d8d4a2e3212e3b2284dc229a1071b400c95cb
unknown <ahmad.rafi02@ui.ac.id>
2021-10-29 19:11:28 +0700

    finishing footer

--- components/landing-page/landingPage.tsx
+++ components/landing-page/landingPage.tsx
@@ -1,34 +1,17 @@
-import Head from 'next/head'
-import { Box } from "@chakra-ui/react"
 import Typewriter from 'typewriter-effect';
 import Link from 'next/link'
 
+import { 
+  Box,
+  Flex,
+  
+} from "@chakra-ui/react"
+
 function LandingPage() {
   return(
-    <section>
-      <div>
-        <h1>JUST TAKE A</h1>
-        <p>
-          <Typewriter 
-          />
-        </p>
-      </div>
-      <div>
-        <Box>
-          SIGN UP
-        </Box>
-        <Box>
-          LOG IN
-        </Box>
-      </div>
-      <div>
-        <Link href="/">
-          <a>
-            log in as a guest
-          </a>
-        </Link>
-     </div>
-    </section>
+    <Box>
+      
+    </Box>
   );
 }
 
