c92d1debb171a7191b76a9d15b29cc69d673defc
unknown <ahmad.rafi02@ui.ac.id>
2021-10-31 13:42:43 +0700

    changing landing page

--- components/landing-page/landingPage.tsx
+++ components/landing-page/landingPage.tsx
@@ -11,102 +11,116 @@ import {
   Heading
 } from "@chakra-ui/react"
 
+/*
+  Todo:
+  benerin sign up dan sign in button
+  buat about as a page jadi pas diklik kayak satu halaman (ide: jadiin beda box)
+*/
+
 
 function LandingPage() {
   return(
-    <Box my='20'>
-      <Container
-        maxW={'3xl'}
-        py='20'
-        my='20'
+    <>
+      <Box my='20'>
+        <Container
+          maxW={'3xl'}
+          py='20'
+          my='20'
+          minH={'100%'}
+        >
+          <Stack 
+            direction={'column'}
+            as={Box}
+            textAlign={'center'}
+            spacing={{ base: 8, md: 14 }}
+            py={{ base: 20, md: 36 }}>
+          <Heading
+            bgGradient="linear(to-l, #0bf8e4,#a409f1, #ec0d1c)"
+            bgClip="text"
+            fontSize="6xl"
+            fontWeight="extrabold"
+            fontFamily={'DMSans'}
+          >JUST TAKE A 
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: true,
+                delay: 40,
+                strings: [
+                "NOTES",
+                "TODO LIST"
+                ]
+              }}
+            />
+          </Heading>
+
+          <Stack 
+            direction={'row'}
+            textAlign={'center'}
+          >
+            <Button  
+              rounded={'full'} 
+              px={{md:20}} 
+              mx={'24'} 
+              bg={'#1F1C2C'} 
+              color={'white'}>
+              SIGN UP</Button>
+
+            <Button 
+              px={{md:20}}
+              rounded={'full'}
+              bg={'#1F1C2C'} 
+              color={'white'}>
+                SIGN IN</Button>
+          </Stack>
+
+          <Link href="/home" passHref>
+            <a>
+              <Text
+                fontSize={{base: '2xl', md: '3xl'}}
+                fontFamily={'DMSans'}
+                fontWeight={'bold'}
+              >log in as a guest</Text>
+            </a>
+          </Link>
+          </Stack>
+        </Container>
+      </Box>
+      <Box
         minH={'100%'}
       >
-        <Stack 
-          direction={'column'}
-          as={Box}
+        <Container
+          w={'100%'}
+          h={'100%'}
+          id='about'
+          py='40'
+          mb='40'
           textAlign={'center'}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}>
-        <Heading
-          bgGradient="linear(to-l, #0bf8e4,#a409f1, #ec0d1c)"
-          bgClip="text"
-          fontSize="6xl"
-          fontWeight="extrabold"
-          fontFamily={'DMSans'}
-        >JUST TAKE A 
-          <Typewriter
-            options={{
-              autoStart: true,
-              loop: true,
-              delay: 40,
-              strings: [
-               "NOTES",
-               "TODO LIST"
-              ]
-            }}
-          />
-        </Heading>
-
-        <Stack 
-          direction={'row'}
-          textAlign={'center'}
+          maxW={'3xl'}
+          as={Stack}
         >
-          <Button  
-            rounded={'full'} 
-            px={{md:20}} 
-            mx={'24'} 
-            bg={'#1F1C2C'} 
-            color={'white'}>
-            SIGN UP</Button>
-
-          <Button 
-            px={{md:20}}
-            rounded={'full'}
-            bg={'#1F1C2C'} 
-            color={'white'}>
-              SIGN IN</Button>
-        </Stack>
-
-        <Link href="/home" passHref>
-          <a>
-            <Text
-              fontSize={{base: '2xl', md: '3xl'}}
-              fontFamily={'DMSans'}
-              fontWeight={'bold'}
-            >log in as a guest</Text>
-          </a>
-        </Link>
-        </Stack>
-      </Container>
-      <Container
-        id='about'
-        py='26'
-        mb='40'
-        textAlign={'center'}
-        maxW={'3xl'}
-        as={Stack}
-      >
-        <Heading
-          my='10'
-          as='h1'
-          textAlign={'center'}
-          fontFamily={'DMSans'}
-          fontSize={{base:'2xl', md:'4xl'}}
-        >About This Web</Heading>
-        <Text
-          letterSpacing='wide'
-          fontFamily={'DMSans'}
-          fontSize={{base:'xl', md: '2xl'}}
-          fontWeight={'semibold'}
-        >
-          This web is all about to save everything, like college notes, todolist,
-          or some kind of database. The very first version of this web is I can only
-          save notes and todolist. The future update about what things could update 
-          here is very very soon.
-          Enjoy. Peace out. -Rf
-        </Text>
-      </Container>
-    </Box>
+          <Heading
+            my='10'
+            as='h1'
+            textAlign={'center'}
+            fontFamily={'DMSans'}
+            fontSize={{base:'2xl', md:'4xl'}}
+          >About This Web</Heading>
+          <Text
+            letterSpacing='wide'
+            fontFamily={'DMSans'}
+            fontSize={{base:'xl', md: '2xl'}}
+            fontWeight={'semibold'}
+          >
+            This web is all about to save everything, like college notes, todolist,
+            or some kind of database. The very first version of this web is I can only
+            save notes and todolist. The future update about what things could update 
+            here is very very soon.
+            Enjoy. Peace out. -Rf
+          </Text>
+        </Container>
+      </Box>
+    </>
   );
 }
 
