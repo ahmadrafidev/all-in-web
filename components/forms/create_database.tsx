9d20de4db9b14a800ecc14324f489259c7fe577c
ahmadrafidev <ahmad.rafi02@ui.ac.id>
2021-12-14 00:41:51 +0700

    add function in 2 two components

--- components/forms/create_database.tsx
+++ components/forms/create_database.tsx
@@ -1,1 +1,12 @@
-export {}
+import {
+    Box
+} from '@chakra-ui/react';
+
+
+export default function Database(){
+    return(
+        <Box>
+            Database form page here
+        </Box>
+    );
+}
