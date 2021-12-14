eec49bb0f31d35f5fdfc3ddb2f1a03558a76cf9b
ahmadrafidev <ahmad.rafi02@ui.ac.id>
2021-12-15 02:49:14 +0700

    initialize todo page

--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,1 +1,23 @@
-export {}
+import Head from 'next/head'
+import { Fragment } from "react";
+import Navbar from '../components/layout/navbarMain';
+import Footer from '../components/layout/footer';
+
+
+const NotesPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Save Everything!</title>
+        <meta 
+          name='description'
+          content='a website to save everything'
+        />
+      </Head>
+      <Navbar/>
+      <Footer/>
+    </Fragment>
+  );
+}
+
+export default NotesPage
